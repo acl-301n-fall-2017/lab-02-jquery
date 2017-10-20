fcdeced9ccac8c297bd923acf2d026cc30b7745d
Langston McClarron <langstunned@gmail.com>
2017-10-19 19:27:12 -0700

    removed template class

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -14,6 +14,7 @@ function Article (rawDataObj) {
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+  $newArticle.removeClass('template');
   /* TODO: This cloned article still has a class of template.
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
