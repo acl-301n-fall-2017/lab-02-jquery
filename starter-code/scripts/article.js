921aad43b5a6b4c396215fb1f910171cf8096180
William Tronvig <wdtronvig289840@gmail.com>
2018-04-29 14:53:49 -0700

    switching drivers

--- starter-code/scripts/article.js
+++ starter-code/scripts/article.js
@@ -3,12 +3,19 @@
 var articles = [];
 
 function Article (rawDataObj) {
-  // TODO: Use the JS object passed in to complete this constructor function:
+  // TODOne: Use the JS object passed in to complete this constructor function:
   // Save ALL the properties of `rawDataObj` into `this`
+  this.title = rawDataObj.title,
+  this.category = rawDataObj.category,
+  this.author = rawDataObj.author,
+  this.authorUrl = rawDataObj.Url,
+  this.publishedOn = rawDataObj.publishedOn,
+  this.body = rawDataObj.body
 }
 
 Article.prototype.toHtml = function() {
   var $newArticle = $('article.template').clone();
+    $('article.template').removeClass(['template']);
   /* TODO: This cloned article still has a class of template.
   However, in our modules.css stylesheet, we gave all elements
   with a class of template a display of none. Let's make
